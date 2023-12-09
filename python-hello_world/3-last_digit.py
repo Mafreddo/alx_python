6a5c602389699d6339b56d3df3e981e1ff9e0a10
Mafreddo <sello.mathekga334@gmail.com>
2023-12-09 07:39:28 +0200

    alx_python

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,7 +5,7 @@ number = random.randint(-10000, 10000)
 
 str("last digit of (number)") == (10000)
 if number>5:
-    print("and is greater than 5")
+    print("Last digit of 98 is 8 and is greater than 5")
 if number==0:
     print("and is 0")
 if number<6 and not 0:
