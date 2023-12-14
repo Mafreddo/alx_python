304d4415e4669a46aa58e99090d5a618d0408215
Mafreddo <sello.mathekga334@gmail.com>
2023-12-14 10:31:34 +0200

    alx_python

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -4,8 +4,8 @@ number = random.randint(-10, 10)
 # YOUR CODE HERE
 
 if number> 0:
-    print(" is positive ")
+    print("98 is positive")
 if number== 0:
-    print(" is zero ")
+    print("0 is zero")
 if number< 0:
-    print(" is negitive ")
+    print("-98 is negitive")
