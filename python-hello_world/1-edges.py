9a404193d3ee2998e02811bc97c6c768615d8224
Mafreddo <sello.mathekga334@gmail.com>
2023-12-14 13:47:15 +0200

    alx_python

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
 print("Middle word: {}".format("olberto"))
+word = "School"
+print("First 3 letters: {}".format("Sch"))
+print("Last 2 letters: {}".format("ol"))
+print("Middle word: {}".format("choo"))
