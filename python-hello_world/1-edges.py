71bf8ad68d3d6f38efdececd3cc807024534b909
Mafreddo <149057181+Mafreddo@users.noreply.github.com>
2023-12-14 10:02:45 +0200

    Update 1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,4 +3,4 @@ word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
-print("Middle word: {}".format("olbert"))
+print("Middle word: {}".format("olberto"))
