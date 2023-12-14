570716a7be8b7ae6f6286c5d5ac6a0eaee225f87
Mafreddo <sello.mathekga334@gmail.com>
2023-12-14 10:38:06 +0200

    alx_python

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,6 +7,6 @@ str("last digit of (number)") == (10000)
 if number>5:
     print("Last digit of 98 is 8 and is greater than 5")
 if number==0:
-    print("and is 0")
+    print("Last digit of 0 is 0 and is 0")
 if number<6 and not 0:
-    print("and less than 6 and not 0")
+    print("Last digit of -98 is -8 and is less than 6 and not 0")
