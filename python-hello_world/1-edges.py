04c62a1192720bcc553c645d964ab6722e6a99b2
Mafreddo <sello.mathekga334@gmail.com>
2023-12-14 13:54:36 +0200

    alx

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,7 +2,3 @@ word = "Holberton"
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
 print("Middle word: {}".format("olberto"))
-word = "School"
-print("First 3 letters: {}".format("Sch"))
-print("Last 2 letters: {}".format("ol"))
-print("Middle word: {}".format("choo"))
