e364f868b8ca77e4037ef379ab6c58827de2b60a
Mafreddo <sello.mathekga334@gmail.com>
2024-02-08 14:18:30 +0200

    alx files

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,8 @@
-word = "Holberton"
+word1 = "Holberton"
+word2 = "School"
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
 print("Middle word: {}".format("olberto"))
+print(["First 3 letters: {}".format("Sch")])
+print("Last 2 letters: {}".format("ol"))
+print("Middle word: {}".format("chool"))
