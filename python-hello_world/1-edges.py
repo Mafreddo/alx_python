fea511d998007a4a6e3db4726e412287091b60ab
Mafreddo <sello.mathekga334@gmail.com>
2024-02-08 14:20:55 +0200

    x

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,6 +3,6 @@ word2 = "School"
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
 print("Middle word: {}".format("olberto"))
-print(["First 3 letters: {}".format("Sch")])
+print("First 3 letters: {}".format("Sch"))
 print("Last 2 letters: {}".format("ol"))
 print("Middle word: {}".format("chool"))
