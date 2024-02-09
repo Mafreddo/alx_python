b916f2608060d7b25714ff1cd942927dcb0d826d
Mafreddo <sello.mathekga334@gmail.com>
2024-02-09 20:59:45 +0200

    Alx

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,5 +1,9 @@
 if __name__== '__main__':
-    print("argv")
-def argv(argv):
-    print(argv)
-argv("1:Hello")
+    def argv(argv):
+        print(argv)
+    argv("1:Hello")
+    argv("2:Helberton")
+    argv("3:School")
+    argv("4:98")
+    argv("5:Bettery")
+    argv("6:street")
