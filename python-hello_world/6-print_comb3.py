09f7c264ba380df9c11a03644375251fcf2f2800
Mafreddo <sello.mathekga334@gmail.com>
2024-02-21 22:22:47 +0200

    learn

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,2 +1,8 @@
-for number in range(00,90):
-    print(number, end=", \t")
+for i in range(10):
+    for j in range(10):
+        if i != j:
+            if i < j:
+                 if i == 8 and j == 9:
+                      print("{:d}{:d}".format(i, j))
+                 else:
+                     print("{:d}{:d}, ".format(i, j), end="")
