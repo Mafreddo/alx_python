39e6aa09496c878d530c621345c70d8e5f8198bd
Mafreddo <sello.mathekga334@gmail.com>
2024-02-21 22:20:12 +0200

    mark the spot

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,2 +1,5 @@
-for i in range(100):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+for x in range(100):
+    if x < 99:
+        print(f"{i:02d}", end=", " if i < 99 else "\n")
+    else :
+        print('{:02d}'.format(x))
