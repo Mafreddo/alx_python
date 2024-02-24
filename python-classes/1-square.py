a7cdd6138fe6bebf97b778e716ef83beac63e87a
Mafreddo <sello.mathekga334@gmail.com>
2024-02-24 11:38:37 +0200

    All Updated

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,41 +1,44 @@
-## Class for returning the size of a aquare
-class Square():
+"""
+This module defines a Square class.
+
+The Square class represents a square geometric shape and has a private
+instance attribute for the size.
+
+Example:
+    square = Square(5)
+    print(square.get_area())  # Output: 25
+"""
+
+class Square:
+    """This class defines a Square.
+
+    Attributes:
+        __size (int): Private instance attribute representing the size of the square.
+    """
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+        """Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+            size (int, optional): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        
+        type(size) == int
+        if type(size) != int:
+            raise TypeError("size must be an integer") 
         elif size < 0:
-            raise ValueError ('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
-if __name__ == '__main__':
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,28 +1,54 @@
-## defining the area of a square
-class Square():
+"""
+This module defines a Square class.
+
+The Square class represents a square geometric shape and has a private
+instance attribute for the size.
+
+Example:
+    square = Square(5)
+    print(square.get_area())  # Output: 25
+"""
+
+class Square:
+    """This class defines a Square.
+
+    Attributes:
+        __size (int): Private instance attribute representing the size of the square.
+    """
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+        """Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+            size (int, optional): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        
+        type(size) == int
+        if type(size) != int:
+            raise TypeError("size must be an integer") 
         elif size < 0:
-            raise ValueError ('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        
+            
+# Public instance method: def area(self): that returns the current square area
+ 
     def area(self):
+        """Calculates area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
-if __name__ == '__main__':
-    my_square_1 = Square(3)
-    print("Area: {}".format(my_square_1.area()))
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    my_square_2 = Square(5)
-    print("Area: {}".format(my_square_2.area()))
