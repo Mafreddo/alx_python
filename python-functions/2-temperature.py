de048e6c1280d534f153bb86e01a110b3d5cc706
Mafreddo <sello.mathekga334@gmail.com>
2024-02-27 17:48:51 +0200

    Update

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,10 +1,3 @@
-unit = input("is this temperatire in Celsius or in Fahrengeit (C/F)")
-temp = float(input("Enter the temperature:: "))
-if unit == "C":
-    temp =round((9 * temp)/5+32, 2)
-    print(f"temperatire in Fahrenheit is: {temp}°C")
-elif unit =="F":
-    temp = round((temp-32)* 5/9, 2)
-    print(f"temperatire in Celsius is: {temp}°F")
-else:
-    print(f"{unit} is an invalid unit of measurement")
+def convert_to_celsius(fahrenheit):
+    convert_to_celsius = (fahrenheit - 32) * 5/9
+    return convert_to_celsius
